386368b23ced60e112fe63e85e5972bf0891314d
everettmorgan <admin@ejmorgan.com>
2022-03-01 12:44:05 -0800

    add comments + move LinkedList.remove to Node.remove

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import pprint
-
-
 def addr(thing):
     if thing is None:
         return 'None'
@@ -9,6 +6,10 @@ def addr(thing):
 
 
 class Node:
+    """
+    a standard circular Node implementation
+    """
+
     def __init__(self, data):
         self.data = data
         self.next = None
@@ -17,38 +18,53 @@ class Node:
     def __str__(self):
         return f'Node<{addr(self)}>: data={self.data} next={addr(self.next)} prev={addr(self.prev)}'
 
+    def remove(self):
+        """
+        removes any next or prev references, and thus itself
+        """
+        if self.next is not None and self.prev is not None:
+            self.next.prev = self.prev
+            self.prev.next = self.next
+        if self.next is not None:
+            self.next.prev = None
+        if self.prev is not None:
+            self.prev.next = None
+        self.prev = None
+        self.next = None
+
 
 class LinkedList:
+    """
+    a standard circular LinkedList implementation
+    """
+
     def __init__(self):
         self.head = None
         self.tail = None
 
     def purge(self, data):
+        """
+        removes all Nodes where Nodes.data == data
+        """
         curr = self.find(data)
 
-        while curr is not None:
-            self.remove(curr)
+        while curr is not None and curr.data == data:
+            curr.remove()
             curr = self.find(data)
 
-    def remove(self, node):
-        if type(node) is Node:
-            raise Exception(f'Expected Node, received {type(node)}')
-        else:
-            if node == self.head:
-                self.head = None
-            else:
-                if node.next is not None:
-                    node.next.prev = node.prev if node.prev is not None else None
-                if node.prev is not None:
-                    node.prev.next = node.next if node.next is not None else None
-
     def find(self, data):
+        """
+        finds and returns the first Node where Node.data == data
+        """
         curr = self.head
         while curr is not None and curr.data != data:
             curr = curr.next;
         return curr
 
     def pop(self):
+        """
+        removes the topmost Node from the LinkedList
+        """
         if self.head is None:
             return
         else:
@@ -58,6 +74,9 @@ class LinkedList:
                 self.head = self.head.next
 
     def push(self, data):
+        """
+        creates and pushes a new Node into the LinkedList
+        """
         new = Node(data)
 
         if self.head is None:
@@ -74,25 +93,38 @@ class LinkedList:
             new.prev = self.tail
 
     def print(self):
-        pos = 1
+        """
+        prints a linked list in its entirety
+        """
+        out = 0
         curr = self.head
-        while curr is not None:
+        while curr is not None and out == 0:
             print(curr)
             curr = curr.next
-            pos += 1
+            if curr is self.head:
+                out = 1
 
 
 if __name__ == '__main__':
     ll = LinkedList()
+
     ll.push(1)
-
     ll.push(2)
     ll.push(2)
-    ll.push(2)
-    ll.push(2)
-
     ll.push(3)
+    ll.push(3)
+    ll.push(3)
+    ll.push(3)
+    ll.push(4)
+    ll.push(4)
+    ll.push(5)
+    ll.push(5)
+    ll.push(5)
+    ll.push(6)
+    ll.push(6)
+    ll.push(6)
+    ll.push(6)
 
-    # ll.purge(2)
+    ll.purge(4)
 
     ll.print()
