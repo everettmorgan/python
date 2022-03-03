ee480b295fc6e6a0cf561ae66509828999a939c1
everettmorgan <admin@ejmorgan.com>
2022-03-02 18:54:13 -0800

    add singleton design pattern

---
+++ Singleton.py
@@ -0,0 +1,44 @@
+class Singleton:
+    instances = {}
+
+    def __new__(cls, clsname, bases, attrs):
+        if clsname not in cls.instances:
+            cls.instances[clsname] = type(clsname, bases, attrs)
+        return cls.instances[clsname]
+
+
+class Foo(Singleton):
+    def __new__(cls, data):
+        if cls.__name__ not in cls.instances:
+            Singleton.__new__(cls, cls.__name__, (), {})
+            cls.instances[cls.__name__] = type(cls.__name__, (), {})
+            cls.instances[cls.__name__].data = data
+        return cls.instances[cls.__name__]
+
+
+if __name__ == '__main__':
+    sa = Foo(1)
+    sb = Foo(2)
+    sc = Foo(3)
+
+    print(sa.data)
+    print(sb.data)
+    print(sc.data)
+
+    sa.data = 2
+
+    print(sa.data)
+    print(sb.data)
+    print(sc.data)
+
+    sb.data = 3
+
+    print(sa.data)
+    print(sb.data)
+    print(sc.data)
+
+    sa.data = 4
+
+    print(sa.data)
+    print(sb.data)
+    print(sc.data)
